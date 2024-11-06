ccdbcd9bc33e7dcbcad121fdb6a97f1dbdbc88e5
KenLimansyah <eonvalleron@gmail.com>
2024-11-06 19:57:38 +0700

    tambah binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ken.abel.listview"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ken.abel.listview"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
--- app/src/main/java/ken/abel/listview/MainActivity.kt
+++ app/src/main/java/ken/abel/listview/MainActivity.kt
@@ -1,19 +1,26 @@
 package ken.abel.listview
 
+import android.app.ActivityManager
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ken.abel.listview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var data = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -27,25 +34,37 @@ class MainActivity : AppCompatActivity() {
             this,
             android.R.layout.simple_list_item_1, data
         )
-        val _lv1 = findViewById<ListView>(R.id.lv1)
-        _lv1.adapter = lvAdapter
+//        val _lv1 = findViewById<ListView>(R.id.lv1)
+//        _lv1.adapter = lvAdapter
+//
+//        val _btnTambah = findViewById<Button>(R.id.btnTambah)
+//        _btnTambah.setOnClickListener {
+//            var dtAkhir = Integer.parseInt(data.get(data.size-1)) + 1
+//            data.add(dtAkhir.toString())
+//
+//            lvAdapter.notifyDataSetChanged()
+//
+//        }
+//        _lv1.setOnItemClickListener(){ parent, view, position, id ->
+//            Toast.makeText(this, "${data[position]}",
+//                Toast.LENGTH_LONG).show()
+//        }
+//        val _btnHapus = findViewById<Button>(R.id.btnHapus)
+//        _btnHapus.setOnClickListener {
+//            data.removeFirst()
+//            lvAdapter.notifyDataSetChanged()
+//            binding.lv1.adapter = lvAdapter
+//        }
 
-        val _btnTambah = findViewById<Button>(R.id.btnTambah)
-        _btnTambah.setOnClickListener {
-            var dtAkhir = Integer.parseInt(data.get(data.size-1)) + 1
-            data.add(dtAkhir.toString())
-
-            lvAdapter.notifyDataSetChanged()
-
-        }
-        _lv1.setOnItemClickListener(){ parent, view, position, id ->
-            Toast.makeText(this, "${data[position]}",
-                Toast.LENGTH_LONG).show()
-        }
-        val _btnHapus = findViewById<Button>(R.id.btnHapus)
-        _btnHapus.setOnClickListener {
-            data.removeFirst()
-            lvAdapter.notifyDataSetChanged()
-        }
+        binding.btnSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                lvAdapter.filter.filter(query)
+                return false
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                lvAdapter.filter.filter(newText)
+                return false
+            }
+        })
     }
 }
